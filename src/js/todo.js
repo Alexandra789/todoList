c93f083c9e06e08a357adfe0a435d2f4796f3bd7
unknown <zakovinko99@gmail.com>
2021-07-14 11:27:44 +0300

    refactor code

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,18 +1,37 @@
 let inputButton = document.querySelector('.input__button');
-let inputText = document.querySelector('.input__text').value;
+let inputText = document.querySelector('.input__text');
+let tasksWrapper = document.querySelector('.tasks-wrapper');
+let task = document.querySelectorAll('.task');
 let error = document.querySelector('.error');
-let deleteButton = document.querySelector('.task__button-delete');
+let deleteButton = document.querySelectorAll('.task__button-delete');
+let title = document.querySelector('.todo-list__title');
 
+title.innerText +=' '+task.length;
+let countTask = task.length;
 inputButton.addEventListener('click', ()=>{
-    if(inputText === ''){
+
+    if(inputText.value === ''){
         error.innerText = 'Enter your task';
     }
     else{
-        console.log('dd');
-        let task = document.createElement('div');
-        task.classList.add("task");
-        task.innerHTML =
-            "<label class=\"task__title\"><input type=\"checkbox\">{inputText}</label>\n" +
-            "<button class=\"button task__button-delete\">Delete</button>";
+        error.innerText = '';
+        let newTask = document.createElement('div');
+        newTask.classList.add("task");
+        newTask.innerHTML =
+            `<label class=\"task__title\"><input type=\"checkbox\">${inputText.value}</label>\n` +
+            `<button class=\"button task__button-delete\">Delete</button>`;
+        tasksWrapper.appendChild(newTask);
+        inputText.innerText += '';
+        title.innerText = `Task ${++countTask}`;
+        deleteButton = document.querySelectorAll('.task__button-delete');
     }
 })
+
+for (let i = 0; i < deleteButton.length; i++) {
+    deleteButton[i].addEventListener('click', ()=> {
+        deleteButton = document.querySelectorAll('.task__button-delete');
+        deleteButton[i].parentNode.remove();
+        title.innerText = `Task ${--countTask}`;
+    })
+}
+
